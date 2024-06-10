78e88eef574bd12b4aeb5b682bd703f650c5a31a
Engr_jalakhan <jk9494173@gmail.com>
2024-06-10 14:33:12 -0700

    COMMIT

--- Patient/MyAppointmentScreen.js
+++ Patient/MyAppointmentScreen.js
@@ -1,150 +1,103 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   View,
   Text,
   Image,
   StyleSheet,
   TouchableOpacity,
-  ScrollView,
-  TextInput,
 } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 
-const MyAppointmentsScreen = ({route, navigation}) => {
-  const {profileImage, userName} = route.params;
+const MyAppointmentsScreen = ({ route, navigation }) => {
   const [bookings, setBookings] = useState([]);
 
   useEffect(() => {
     const fetchBookings = async () => {
       try {
-        // Fetch top doctor IDs from TopDoctor collection
         const bookingsSnapshot = await firestore()
           .collection('Bookings')
           .get();
-        const doctorId = bookingsSnapshot.docs.map(
-          doc => doc.data().doctorId,
-        );
 
-        // Fetch detailed doctor data from Doctor collection
-        const doctorPromises = doctorId.map(async id => {
+        const bookingsData = [];
+        for (const bookingDoc of bookingsSnapshot.docs) {
+          const bookingData = bookingDoc.data();
+          const doctorId = bookingData.doctorId;
+
           const doctorDoc = await firestore()
             .collection('Doctor')
-            .doc(id)
+            .doc(doctorId)
             .get();
-          return {id: doctorDoc.id, ...doctorDoc.data()};
-        });
 
-        const doctorsList = await Promise.all(doctorPromises);
-        setBookings(doctorsList);
+          if (doctorDoc.exists) {
+            const doctorData = doctorDoc.data();
+            bookingsData.push({ bookingId: bookingDoc.id, doctorData });
+          } else {
+            console.log(`Doctor with ID ${doctorId} does not exist.`);
+          }
+        }
+
+        setBookings(bookingsData);
       } catch (error) {
-        console.error('Error fetching top doctors:', error);
+        console.error('Error fetching bookings:', error);
       }
     };
 
     fetchBookings();
   }, []);
 
-  // const handlePress = option => {
-  //   navigation.navigate(option, {
-  //     profileImage: profileImage,
-  //     userName: userName,
-  //   });
-  // };
   const handleViewProfile = (doctorId) => {
     navigation.navigate('FeedbackScreen', { doctorId });
   };
 
   const handleBookAppointment = () => {
-    // Implement booking functionality here
-    navigation.navigate("Book")
+    navigation.navigate('Book');
   };
 
   const handleCallDoctor = () => {
     // Implement calling functionality here
   };
-  const [bookmarkedDoctors, setBookmarkedDoctors] = useState([]);
-
-  const handleBookmarkDoctor = async (doctor) => {
-    try {
-      // Check if the doctor is already bookmarked
-      const bookmarkSnapshot = await firestore()
-        .collection('BookmarkedDoctors')
-        .where('id', '==', doctor.id)
-        .get();
-  
-      if (!bookmarkSnapshot.empty) {
-        // Doctor is already bookmarked, remove it
-        const docId = bookmarkSnapshot.docs[0].id;
-        await firestore().collection('BookmarkedDoctors').doc(docId).delete();
-        setBookmarkedDoctors(prevBookmarkedDoctors =>
-          prevBookmarkedDoctors.filter(bookmarkedDoctor => bookmarkedDoctor.id !== doctor.id)
-        );
-      } else {
-        // Doctor is not bookmarked, add it
-        await firestore().collection('BookmarkedDoctors').add(doctor);
-        setBookmarkedDoctors(prevBookmarkedDoctors => [...prevBookmarkedDoctors, doctor]);
-      }
-    } catch (error) {
-      console.error('Error toggling bookmark for doctor: ', error);
-    }
-  };
 
   return (
     <View style={styles.container}>
-      {bookings.map(doctor => (
-            <View key={doctor.id} style={styles.card}>
-              <View style={styles.profileContainer1}>
-                <Image
-                  source={{uri: doctor.imageUrl}}
-                  style={styles.profileImage1}
-                />
-                <TouchableOpacity onPress={() => handleViewProfile(doctor.id)}>
-                  <Text style={styles.viewProfileButton}>View Feedback</Text>
-                </TouchableOpacity>
-              </View>
-              <View style={styles.doctorDetails}>
-                <Text style={styles.doctorName}>{doctor.name}</Text>
-                <Text style={styles.specialty}>{doctor.specialty}</Text>
-                <Text style={styles.experience}>
-                  Experience: {doctor.experience} year
-                </Text>
-                <Text style={styles.experience}>
-                  Location: {doctor.clinicAddress}
-                </Text>
-                <Text style={styles.experience}>Rasst: {doctor.rasst}</Text>
-                {/* Add more doctor details here */}
-              </View>
-              <View style={styles.buttonContainer}>
-                <View style={styles.bookmarkContainer}>
-                  <TouchableOpacity
-                    onPress={() => handleBookmarkDoctor(doctor)}
-                    style={styles.bookmarkButton}>
-                    <Text
-                      style={[
-                        styles.bookmarkText,
-                        {
-                          color: bookmarkedDoctors.includes(doctor.id)
-                            ? 'gold'
-                            : 'gray',
-                        },
-                      ]}>
-                      🔖
-                    </Text>
-                  </TouchableOpacity>
-                </View>
-                <TouchableOpacity
-                  onPress={handleBookAppointment}
-                  style={styles.actionButton}>
-                  <Text style={styles.actionButtonText}>Book</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  onPress={handleCallDoctor}
-                  style={styles.actionButton}>
-                  <Text style={styles.actionButtonText}>Call</Text>
-                </TouchableOpacity>
-              </View>
+      {bookings.map(({ bookingId, doctorData }) => (
+        <View key={bookingId} style={styles.card}>
+          {doctorData && doctorData.imageUrl && (
+            <View style={styles.profileContainer1}>
+              <Image
+                source={{ uri: doctorData.imageUrl }}
+                style={styles.profileImage1}
+              />
+              <TouchableOpacity onPress={() => handleViewProfile(doctorData.id)}>
+                <Text style={styles.viewProfileButton}>View Feedback</Text>
+              </TouchableOpacity>
             </View>
-          ))}
+          )}
+          <View style={styles.doctorDetails}>
+            <Text style={styles.doctorName}>{doctorData?.name}</Text>
+            <Text style={styles.specialty}>{doctorData?.specialty}</Text>
+            <Text style={styles.experience}>
+              Experience: {doctorData?.experience} year
+            </Text>
+            <Text style={styles.experience}>
+              Location: {doctorData?.clinicAddress}
+            </Text>
+            <Text style={styles.experience}>Rasst: {doctorData?.rasst}</Text>
+            {/* Add more doctor details here */}
+          </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              onPress={handleBookAppointment}
+              style={styles.actionButton}>
+              <Text style={styles.actionButtonText}>Book</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={handleCallDoctor}
+              style={styles.actionButton}>
+              <Text style={styles.actionButtonText}>Call</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      ))}
     </View>
   );
 };
@@ -163,8 +116,8 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 5,
     marginBottom: 50,
-    width:350,
-    justifyContent:'space-between',
+    width: 350,
+    justifyContent: 'space-between',
   },
   profileContainer1: {
     alignItems: 'center',
@@ -196,17 +149,6 @@ const styles = StyleSheet.create({
   actionButtonText: {
     color: 'white',
     textAlign: 'center',
-  },
-  bookmarkButton: {
-    alignItems: 'center',
-    width: 50,
-    height: 50,
-    top: 10,
-    right: 10,
-    paddingLeft:20
-  },
-  bookmarkText: {
-    fontSize: 24,
   },
 });
 
